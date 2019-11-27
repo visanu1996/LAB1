3d00e65705401598b70d007fb69d5c97ca64c740
visanu1996 <workland1@live.com>
2019-11-28 00:08:56 +0700

    added to repo

---
+++ src/main.java
@@ -0,0 +1,13 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String firstName = "Visanu";
+		System.out.println("hello");
+		System.out.println("hello "+firstName);
+		
+		
+	}
+
+}
